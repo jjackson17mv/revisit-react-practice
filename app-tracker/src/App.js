231789a95ed9c0f4523f59f011e9c05a01905671
joshua jackson <jjackson17mv@gmail.com>
2021-03-31 16:12:52 -0700

    integrated code from previous repo, creating Task.js

---
+++ app-tracker/src/App.js
@@ -0,0 +1,36 @@
+import "./App.css";
+import Header from './components/Header';
+import Tasks from './components/Tasks';
+import { useState } from "react";
+
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctors Appointment",
+      day: "Feb 5th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Laundry",
+      day: "Feb 6th at 3:30pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Shopping",
+      day: "Feb 7th at 4:00pm",
+      reminder: false,
+    },
+  ]); 
+  return (
+    <div className="container">
+     <Header/>
+     <Tasks tasks ={tasks}/>
+    </div>
+  );
+}
+
+export default App;
+ 
