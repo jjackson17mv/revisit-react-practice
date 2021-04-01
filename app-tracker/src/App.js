8a9644b3e945664ede89173e5df2d86a42a0475f
joshua jackson <jjackson17mv@gmail.com>
2021-04-01 15:10:53 -0700

    starting doubleClick with ontoggle

--- app-tracker/src/App.js
+++ app-tracker/src/App.js
@@ -24,10 +24,20 @@ const App = () => {
       reminder: false,
     },
   ]); 
+
+  //Delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task)=> task.id !==id))
+  }
+
+  const toggleReminder =(id) => {
+    console.log(id)
+  }
+
   return (
     <div className="container">
      <Header/>
-     <Tasks tasks ={tasks}/>
+     {tasks.length > 0 ?<Tasks tasks ={tasks} onDelete= {deleteTask} onToggle={toggleReminder}/> : 'NO TASK TO SHOW'}
     </div>
   );
 }
--- app-tracker/src/components/Task.js
+++ app-tracker/src/components/Task.js
@@ -1,9 +1,14 @@
-const Task = ({task}) => {
-  return <div className='task'>
-      <h3>{task.text}</h3>
-      <p>{task.day}</p>
+import { FaTimes } from "react-icons/fa";
 
-  </div>;
+const Task = ({ task, onDelete}) => {
+  return (
+    <div className="task" onDoubleClick={() => ontoggle(task.id)}>
+      <h3>
+        {task.text} <FaTimes style={{ color: "red", cursor: "pointer" }} onClick={()=>onDelete(task.id)} />
+      </h3>
+      <p>{task.day}</p>
+    </div>
+  );
 };
 
 export default Task;
